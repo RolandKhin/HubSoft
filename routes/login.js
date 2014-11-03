20a30916df840691b8249ba1659019fed5676445
Kenny Cheang <kcheang@k-swiss.com>
2014-11-03 16:56:22 +0800

    externalized config settings.

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var router = express.Router()
 var soap = require('soap')
-var url = 'https://ws.hubsoft.com/services/SiteService?wsdl'
+var config = require('config')
 
 // login page
 router.get('/', function(req, res) {
   var args = {userName: 'admin', password: 'password', accessKey: '83iJiJNkte'}
-  soap.createClient(url, function(err, client) {
+  soap.createClient(config.hubsoft.soap.url, function(err, client) {
     client.authUser(args, function(err, result) {
       console.log(result)
     })
